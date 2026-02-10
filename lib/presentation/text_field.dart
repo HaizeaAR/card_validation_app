e1bfd87d21216e5ddb80f5378cc1b4be38c4e978
FranciscoMonGar <[200~francisco.montes@interactvty.com>
2026-02-10 10:30:55 +0100

    text_field_adds

--- lib/presentation/text_field.dart
+++ lib/presentation/text_field.dart
@@ -13,8 +13,54 @@ class MessageValidate extends StatelessWidget {
         FilteringTextInputFormatter.digitsOnly // con esta linea haremos que solo permita solo números
   ],
         decoration: const InputDecoration(
-        border: OutlineInputBorder(), 
-        labelText: 'Credit card number'),
+        border: OutlineInputBorder(),
+        labelText: 'Credit card number',
+        hintText: '400 1234 5678 9010',
+        ),
+      ),
+    );
+  }
+}
+
+class MessageCvv extends StatelessWidget {
+  const MessageCvv({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 250,
+      child: TextField(
+        obscureText: true, // con esta linea haremos que el texto se oculte al escribirlo
+        inputFormatters: [
+        LengthLimitingTextInputFormatter(3), // con esta linea haremos que solo permita 3 caracteres
+        FilteringTextInputFormatter.digitsOnly // con esta linea haremos que solo permita solo números
+  ],
+        decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'CVV',
+        hintText: '123',
+        ),
+      ),
+    );
+  }
+}
+
+class MessageName extends StatelessWidget {
+  const MessageName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 250,
+      child: TextField(
+        inputFormatters: [
+        FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z\s]')), // con esta linea haremos que solo permita letras y espacios
+  ],
+        decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Cardholder name',
+        hintText: 'Haizea Ariza Robleda',
+        ),
       ),
     );
   }
