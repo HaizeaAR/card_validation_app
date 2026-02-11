44ad8810973c077dc4da2c22936a826d0b9734fa
HaizeaAR <haizea.ariza@interactvty.com>
2026-02-11 13:00:11 +0100

    hotfix (name of the app)

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Credit Card Form'),
+        title: const Text('Credit Card Validation'),
       ),
       body: SingleChildScrollView(
         child: Column(
