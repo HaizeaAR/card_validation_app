a4ba367be578e8cc5a0e904533c0628843aa534e
HaizeaAR <haizea.ariza@interactvty.com>
2026-02-12 12:54:30 +0100

    fix of the DropList

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,56 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
+class _DropList extends StatefulWidget { 
+@override
+  _DropListState createState() => _DropListState();
+  }
+
+  
+class _DropListState extends State<_DropList> {
+  final items = ['Visa', 'MasterCard', 'American Express', 'Discover', 'Other'];
+String? value;
+
+  @override
+Widget build(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20),
+    child: Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.grey, width: 2),
+      ),
+      child: DropdownButton<String>(
+        value: value,
+        hint: const Text("Select card type"),
+        isExpanded: true,
+        icon: const Icon(Icons.arrow_drop_down),
+        items: items.map(buildMenuItem).toList(),
+        onChanged: (value) => setState(() => this.value = value),
+      ),
+    ),
+  );
+}
+
+
+  DropdownMenuItem<String> buildMenuItem(String item) => 
+  DropdownMenuItem(
+    value: item, 
+    child: Text(
+      item,
+      style:TextStyle(fontSize: 20), 
+      ),
+    ); 
+  }
 
 class _MyHomePageState extends State<MyHomePage> {
 
@@ -47,6 +91,10 @@ class _MyHomePageState extends State<MyHomePage> {
     key: _formKey,
     child: Column(
       children: [
+        _DropList(),
+        
+        const SizedBox(height: 10),
+
         FullName(controller: nameController),
         CreditCard(controller: cardController),
         ExpDate(controller: expController),
