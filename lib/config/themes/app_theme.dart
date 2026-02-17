dd8e8ea9fd9020f56de8fe9958beb0d5f6545ba8
HaizeaAR <haizea.ariza@interactvty.com>
2026-02-17 10:05:42 +0100

    comments on app theme

--- lib/config/themes/app_theme.dart
+++ lib/config/themes/app_theme.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
+//Una constante para guardar un color especifico para usarlo luego dentro del theme
 const Color buttonColor = Color.fromARGB(255, 128, 152, 198);
 
 class AppTheme { 
+  //Creo un theme especifico para el modo claro (será el único modo en esta App)
   static ThemeData get lightTheme {
     return ThemeData(
+      //Color primario de la App
       primarySwatch: Colors.orange,
-
+      //Color de fondo de la App
       scaffoldBackgroundColor: Colors.white,
+      //Aquí creo una función entera para el botón
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
           backgroundColor: const Color.fromARGB(255, 255, 255, 255),
@@ -20,8 +24,11 @@ class AppTheme {
         ),
       ),
       ),
+      //Más decoración para la App 
       inputDecorationTheme: const InputDecorationTheme(
+        //El texto chivato lo coloco en gris (como se ve usualmente en este tipo de apps)
         hintStyle: TextStyle( color: Color.fromARGB(201, 180, 180, 180), fontSize: 16 ),
+        //El borde y texto de los campos de textos lo pongo en un tono verdoso
         border: OutlineInputBorder(),
         labelStyle: TextStyle(
           color: Color.fromRGBO(5, 111, 30, 1),
@@ -30,6 +37,7 @@ class AppTheme {
         focusedBorder: OutlineInputBorder(
           borderSide: BorderSide(color: Color.fromARGB(255, 36, 185, 23), width: 2.0),
         ),
+        //Aquí, con los errores, hago que salga el texto en rojo y el campo pase a verse rojo también para enfatizar el error al usuario
         errorBorder: OutlineInputBorder(
           borderSide: BorderSide(color: Colors.red, width: 2.0),
         ),
